5b67362385c4b504a2b90e86b17f9cdb8bce94fd
JeanKevin56 <148986903+JeanKevin56@users.noreply.github.com>
2023-11-15 18:11:16 +0100

    Adding idf() WITHOUT COMMENTS

--- TF-IDF.py
+++ TF-IDF.py
@@ -7,3 +7,28 @@ def tf(text):
         else:
             tf_dico[word] = 1
     return tf_dico
+
+def idf(directory="./cleaned"):
+    liste_des_mots = {}
+    number_of_files = 0
+    for file in list_of_files(directory, ".txt"):
+        number_of_files += 1
+        with open(directory + "/" + file, "r") as f:
+
+            dico_mot_fichier = tf(f.read())
+            mot_dans_dico = []
+
+            for mot in dico_mot_fichier.keys():
+                if mot not in mot_dans_dico:
+                    mot_dans_dico.append(mot)
+
+                    if mot in liste_des_mots.keys():
+                        liste_des_mots[mot] += 1
+
+                    else:
+                        liste_des_mots[mot] = 1
+
+    for val in liste_des_mots:
+        liste_des_mots[val] = math.log(1 / (liste_des_mots[val] / number_of_files))
+
+    return liste_des_mots
