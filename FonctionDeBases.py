1df65f12bfc860f9818506495d394ff00649e9e8
JeanKevin56 <148986903+JeanKevin56@users.noreply.github.com>
2023-11-15 19:29:03 +0100

    Update get_names

--- FonctionDeBases.py
+++ FonctionDeBases.py
@@ -1,25 +1,30 @@
 from RechercheFichier import *
 
 
-def get_names(directory="./speech/"):
+def get_names(directory = "./speech/", file = -1):
     """
     :param : Dossier contenant les fichier des discours
     :return: list_name: liste des noms de chaque présients ayant fait un discours que l'on posséde en fichier
-
+    
     :description: Recupère les noms de tt les présidents
     """
-    list_files = list_of_files(directory, ".txt")
-    set_of_name = {"Initialisation du set"}
-    set_of_name.clear()
-    for file in list_files:
-        if 47 < ord(file[-5]) < 58:  # Si le 5 eme caractère est un chiffre:
-            set_of_name.add(file[:-5][
-                            11:])  # On ne le prend pas dans le nom car c'est un indicateur du numéro de discourd tel que le 1 dans "Chirac1.txt"
-
-        else:  # Sinon si ce n'est pas un chiffre:
-            set_of_name.add(file[:-4][11:])  # On prend prend le 5eme caractere car il fait parti du nom
-
-    return list(set_of_name)
+    if file == -1:
+        list_files = list_of_files(directory,".txt")
+        set_of_name = {"Initialisation du set"}
+        set_of_name.clear()
+        for file in list_files:
+            if 47 < ord(file[-5]) < 58:             # Si le 5 eme caractère est un chiffre:
+                set_of_name.add(file[:-5][11:])     # On ne le prend pas dans le nom car c'est un indicateur du numéro de discourd tel que le 1 dans "Chirac1.txt"
+            
+            else:                                   # Sinon si ce n'est pas un chiffre:
+                set_of_name.add(file[:-4][11:])     # On prend prend le 5eme caractere car il fait parti du nom
+        
+        return list(set_of_name)
+    else:
+        if 47 < ord(file[-5]) < 58:
+            return (file[:-5][11:])
+        else:                                   
+            return (file[:-4][11:])
 
 
 def associe(list_noms):
