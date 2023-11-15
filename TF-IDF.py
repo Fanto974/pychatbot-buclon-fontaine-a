f8fffae2d0a85082e60d6f0ff91f471536998b53
JeanKevin56 <148986903+JeanKevin56@users.noreply.github.com>
2023-11-15 18:12:35 +0100

    Adding tfidf WITHOUT COMMENTS

--- TF-IDF.py
+++ TF-IDF.py
@@ -32,3 +32,19 @@ def idf(directory="./cleaned"):
         liste_des_mots[val] = math.log(1 / (liste_des_mots[val] / number_of_files))
 
     return liste_des_mots
+    
+def tfidf(directory="./cleaned"):
+    liste_des_mots = idf(directory)
+    matrice_tf_idf = []
+    id = 0
+    for mot in liste_des_mots:
+        matrice_tf_idf.append([])
+        for file in list_of_files(directory, ".txt"):
+            with open(directory + "/" + file, "r") as f:
+                dico_mot_fichier = tf(f.read())
+            if mot in dico_mot_fichier:
+                matrice_tf_idf[id].append(dico_mot_fichier[mot] * liste_des_mots[mot])
+            else:
+                matrice_tf_idf[id].append(0.0)
+        id += 1
+    return matrice_tf_idf
