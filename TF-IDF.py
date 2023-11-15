893b57314722cda2c8f7ebe0c382b54d93bd0171
Fanto974 <144560009+Fanto974@users.noreply.github.com>
2023-11-15 18:09:46 +0100

    Create TF in TF-IDF.py

--- TF-IDF.py
+++ TF-IDF.py
@@ -1,1 +1,9 @@
-
+def tf(text):
+    words = text.split(" ")
+    tf_dico = {}
+    for word in words:
+        if word in tf_dico.keys():
+            tf_dico[word] += 1
+        else:
+            tf_dico[word] = 1
+    return tf_dico
