f20d474f673c204a78a0ee6c0d79ab3de8cb39f0
JeanKevin56 <148986903+JeanKevin56@users.noreply.github.com>
2023-11-23 15:50:21 +0100

    Small update

--- Fonctionalité3.py
+++ Fonctionalité3.py
@@ -1,10 +1,14 @@
+from RechercheFichier import *
+from FonctionDeBases import *
+from TFIDF import *
+
 def Plus_mot(president):
     list_fichier = list_of_files("./cleaned", "txt")
     mot_max = [0, ""]
     for i in range(len(list_fichier)):
         with open("./cleaned/" + list_fichier[i], "r") as fi:
             nom = get_names(-1, list_fichier[i])
-            if nom == president:
+            if minimize_text(nom) == minimize_text(president):
                 text = fi.readline()
                 dico_text = tf(text)
                 for j in list(dico_text.keys()):
