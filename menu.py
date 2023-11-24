a7e00d36ed507c68e43b0aaba6aeb0edaa4a7145
JeanKevin56 <148986903+JeanKevin56@users.noreply.github.com>
2023-11-24 16:46:56 +0100

    Fix input error when mistake by user

--- menu.py
+++ menu.py
@@ -9,22 +9,22 @@ from Fonctionalite6 import *
 
 
 def menu():
-    choice = 1
-    while choice != 0:
+    choice = "1"
+    while choice != "0":
         print("\n\n\n\n\n\n\n\n\n\n\n\n\n")
         line = "______________________________________________________________________________________________"
         print(line)
         print("MENU:\n - 0: Quitter\n - 1: Liste des mots avec un TF-IDF de 0\n - 2: Affiche le mot avec le TF-IDF le plus élevé\n - 3:Affcihe le mot le plus répété par un président\n - 4:Indique le noms des présidents ayant prononcé un certain mot\n - 5:Indique le premier président à parler d'un mot donné\n - 6: Indique les mots dont tout le monde à parlé")
         print(line+"\n\n\n")
-        choice = int(input("Votre choix : "))
-        if choice == 0:
+        choice = input("Votre choix : ")
+        if choice == "0":
             pass
-        elif choice == 1:
+        elif choice == "1":
             print("\nLes mots avec un TF-IDF de 0 sont:")
             for val in NonImportant():
                 print(val, end = ", ")
             print("\n\n")
-        elif choice == 2:
+        elif choice == "2":
             print("Le mot avec le plus élevé TF-IDF est: ")
             high = highest_tfidf()
             print(high[0][0])
@@ -32,11 +32,11 @@ def menu():
             print(high[0][1])
             print("\nPar "+get_names(0, high[0][1]))
             print("\n\n")
-        elif choice == 3:
+        elif choice == "3":
             cible = input("\nQuel président voulez-vous cibler : ")
             print("Le mot le plus prononcé par "+cible+" est : "+Plus_mot(cible))
             print("\n\n")
-        elif choice == 4:
+        elif choice == "4":
             mot = input("\nQuel mot voulez-vous analyser : ")
             who_s = who_said("./cleaned/",mot)
             print("Le mot "+mot+" a été prononcé par :")
@@ -46,18 +46,18 @@ def menu():
             for val in who_s[0]:
                 print(val)
             print("\n")
-        elif choice == 5:
+        elif choice == "5":
             mot = minimize_text(input("\nQuel mot voulez-vous analyser : "))
             print("Le premier président à parler de "+mot+" est : ", end ="")
             for val in premier_mot(mot):
                 print(val)
-        elif choice == 6:
+        elif choice == "6":
             print("A part les mots non importants. \nLes mots que tout les présidents prononcent sont : ")
             mots = NonImportant6()
             for val in mots:
                 print(val, end = ", ")
         else:
             print("Veuillez entrez une autre valeur, cette valeur n'est pas prise en charge.")
-        if choice != 0:
+        if choice != "0":
             pause = input()
 menu()
