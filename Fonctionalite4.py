9c422e1a18fd6bfacde27247fa398655ad84e67f
JeanKevin56 <148986903+JeanKevin56@users.noreply.github.com>
2023-11-24 17:22:28 +0100

    Remake of the function
    J'ai refait le fonction en utilisant le tf de chaque fichiers au lieu du tf-idf car les mots dit par tout le monde était donc tous à 0, donc il étaiot compliqué de voir qui l'avait prononcé le plus.

--- Fonctionalite4.py
+++ Fonctionalite4.py
@@ -1,31 +1,29 @@
 from TFIDF import *
 from FonctionDeBases import *
+from Fonctionalite1 import *
 
 def who_said(directory="./cleaned/",word="Nation"):
     """
     :desc: Fonction qui dit quel président on prononcer un mot donné
     """
+    word = minimize_text(word)             #Enleve les majuscules
     tf_idf = tfidf(directory)               #La matrice Tf-IDF
     dico_key = list(idf().keys())           #La liste de tout les mots de TF-IDF
     liste_names = get_names(directory, 0, 0)      #Le nom de tout les présidents
     time_said = {}                          
     people_max = []
-
     #La valeur maxi et l'id auxquels il est associé
     maxi = 0
     id = 0
+    for file in list_of_files(directory, ".txt"):
+        name = get_names(0, file)
+        with open(directory + file, "r") as f:
+            dico = tf(f.read())
+            for val in dico:
+                print(val)
+                if val == word:
+                    time_said[name] = dico[word]
 
-    for i in range(len(dico_key)-1):                            #Pour chaque mot dans le dictionnaire
-        if minimize_text(dico_key[i]) == minimize_text(word):   #Si le mot est celui que l'on recherche
-            id = i                                              #On note son id puis on quitte la boucle
-            break
-
-    for i in range(len(tf_idf[id])):                    #Pour chaque documents dans le ligne TF-IDF du mot cherché
-        if tf_idf[id][i]!=0:
-            if liste_names[i] in time_said.keys():          #Si le nom du président est déjà dans la liste de ceux qui ont dit le mot
-                time_said[liste_names[i]] += tf_idf[id][i]  #
-            else:
-                time_said[liste_names[i]] = tf_idf[id][i]          
     for val in time_said.keys():
         if time_said[val] > maxi:
             maxi = time_said[val]
