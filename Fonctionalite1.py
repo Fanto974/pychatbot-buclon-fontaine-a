0ce554f65a66aa20f052a69e955fb184da63f23c
Fanto974 <144560009+Fanto974@users.noreply.github.com>
2023-11-24 17:08:03 +0100

    Comment Fonctionalite1.py

--- Fonctionalite1.py
+++ Fonctionalite1.py
@@ -12,7 +12,7 @@ def NonImportant():
     list_mots_non_important = []                                 # Enfin, on crée une liste qui va contenir tous les mots dit non importants. PS : c'est la variable que l'on va renvoyer
     for liste in range(len(matrice)):                            # On parcours par index toute la matrice
         occurence_mot = 0                                        # On initalise a chaque tour de boucle, soit a chaque nouveau mot une variable qui va compter le nombre de texte dans lesquels le mots n'a pas un tfidf de 0.
-        for val in range(len(matrice[liste])):                                     # Comme il y a 8 textes on va donc regarder 8 valeurs différentes
+        for val in range(len(matrice[liste])):                   # On va regarder pour chaque mot la valeur de son tfidf
             if matrice[liste][val] != 0:                         # Si le mot dans le texte que l'on regarde a un tfidf différent de 0 on ajoute 1 a la variable occurence_mot
                 occurence_mot += 1
         if occurence_mot == 0:                                   # Après avoir parcourus les 8 valeurs d'un mot, si la variable occurence_mot est toujours a 0 c'est que pour chaque texte, ce mot a un tfidf de 0. Il n'est donc pas important
