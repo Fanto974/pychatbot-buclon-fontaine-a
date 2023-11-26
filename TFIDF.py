a6eb5851c104fcef6243ddcee17fad6d7c8a24c1
JeanKevin56 <148986903+JeanKevin56@users.noreply.github.com>
2023-11-26 13:18:30 +0100

    comment idf()

--- TFIDF.py
+++ TFIDF.py
@@ -10,27 +10,28 @@ def tf(text):
     return tf_dico
 
 def idf(directory="./cleaned"):
-    liste_des_mots = {}
+    liste_des_mots = {}                                 
     number_of_files = 0
-    for file in list_of_files(directory, ".txt"):
-        number_of_files += 1
-        with open(directory + "/" + file, "r") as f:
+    for file in list_of_files(directory, ".txt"):       #Pour chaque fichier dans le repertoire donné:
+        number_of_files += 1                            #Le nbr de fichier qu'on a regadé +=1
+        with open(directory + "/" + file, "r") as f:    #On ouvre le fichier pour le lire
 
-            dico_mot_fichier = tf(f.read())
+            dico_mot_fichier = tf(f.read())             #On recup le tf du fichier voulu, que l'on utilise comme une liste de tt le mot du fichier
             mot_dans_dico = []
 
-            for mot in dico_mot_fichier.keys():
-                if mot not in mot_dans_dico:
-                    mot_dans_dico.append(mot)
+            for mot in dico_mot_fichier.keys():         #Pour chaque mot dans le fichier:
+                if mot not in mot_dans_dico:            #Si le mot n'est pas déjà dans la liste des mot mis dans le dico (Si c'est la 1ere fois du fichier qu'on tombe sur ce mot)
+                    mot_dans_dico.append(mot)           #On ajout le mot dans la liste (Liste qui sera vidé pour le prochain fichier)
+                    
+                    #c'est donc la seul fois du fichier qu'on traitera ces mots
+                    if mot in liste_des_mots.keys():    #Si le mot est dejà dans la liste des mots
+                        liste_des_mots[mot] += 1        #On ajoute 1 à son occurence
 
-                    if mot in liste_des_mots.keys():
-                        liste_des_mots[mot] += 1
+                    else:                               #Sinon
+                        liste_des_mots[mot] = 1         #On associe sa valeur à 1
 
-                    else:
-                        liste_des_mots[mot] = 1
-
-    for val in liste_des_mots:
-        liste_des_mots[val] = math.log(1 / (liste_des_mots[val] / number_of_files))
+    for val in liste_des_mots:                          #Pour chaque valeur dans la liste des mots:
+        liste_des_mots[val] = math.log(1 / (liste_des_mots[val] / number_of_files)) #On remplace sa valeur par de log de l'inverse de sa proportion (C'est là qu'on utilise  le comptage de doculments fait plus haut)
 
     return liste_des_mots
     
