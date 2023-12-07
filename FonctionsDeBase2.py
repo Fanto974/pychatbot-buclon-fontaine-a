afe2e2a02720483d57537657dbf11f467665881f
JeanKevin56 <148986903+JeanKevin56@users.noreply.github.com>
2023-12-07 15:47:10 +0100

    Adding response
    Ajout de la fct qui repond à l'utilisateur
    Ajout d'un fct qui renvoie une liste qui trie les mots de la question dans l'ordre décroissant en fonction de leur TF-IDF
    Ajout d'un fonction de tri par selection qui trie une fonction indice à indice en se basant sur une autre fonction contenant les valeurs

--- FonctionsDeBase2.py
+++ FonctionsDeBase2.py
@@ -56,19 +56,18 @@ def compose_matrice(matrice):
             new_matrice[j][i] = matrice[i][j]
     return new_matrice
 
-def TFIDF_Qestion(text):
+def TFIDF_Qestion(text, directory = "./cleaned"):
     M = []
-    list_mots = list(idf("./cleaned").keys())
+    list_mots = list(idf(directory).keys())
     list_mots_Question = intersection(text)
     tf_motQuestion = tf(text)
-    idf_motCorpus = idf("./cleaned")
+    idf_motCorpus = idf(directory)
     for i in list_mots:
         if i in list_mots_Question:
             M.append(tf_motQuestion[i] * idf_motCorpus[i])
         else:
             M.append(0)
     return M
-#print(TFIDF_Qestion("bonjour jour jour messieurs abaissement dames le climat change"))
 
 def norme(vector):
     sum = 0
@@ -98,6 +97,7 @@ def similar(matrice_question,directory = "./cleaned"):
     for i in range(len(matrice)):
         list_similarities.append(scalaire(matrice_question, matrice[i])/norme_q*list_norme[i])
     return list_similarities
+#print(similar(TFIDF_Qestion("les doit est bien")))
 
 def doc_pertinent(matrice_question, list_nomFichier = list_of_files("./cleaned", "txt")):
     liste_similarite = similar(matrice_question)
@@ -110,7 +110,6 @@ def doc_pertinent(matrice_question, list_nomFichier = list_of_files("./cleaned",
         return "Il n'y a aucun document plus pertinent"
     else:
         return list_nomFichier[max[1]]
-#print(doc_pertinent(TFIDF_Qestion("les doit est bien")))
 
 def most_impo_q(text):
     max = 0
@@ -121,4 +120,61 @@ def most_impo_q(text):
             max = question_tfidf[i]
             id = i
     return list(idf().keys())[id]
-#print(most_impo_q("bonjour jour doit messieurs abaissement dames le climat change"))
+    
+def l_most_impo_q(text):
+    question_tfidf = TFIDF_Qestion(text)
+    l_tfidf = list(idf().keys())
+    list_question_tfidf = []
+    text = tokenisation(text)
+    for val in text:
+        found = False
+        for i in range(len(l_tfidf)):
+            if val == l_tfidf[i]:
+                list_question_tfidf.append(question_tfidf[i])
+                found = True
+                break
+        if not found:
+            list_question_tfidf.append(0.0)
+    #for i in range(len(text)):
+    #    print(str(text[i])+ "--->"+str(list_question_tfidf[i]))
+    return tri_selec(list_question_tfidf, text)
+
+
+def tri_selec(l,m):
+    final_l = []
+    for i in range(len(l)):
+        id_min = i
+        for j in range(id_min+1, len(l)):
+            if l[j] < l[id_min]:
+                id_min = j
+        l[i],l[id_min] = l[id_min],l[i]
+        m[i],m[id_min] = m[id_min],m[i]
+    for i in range(len(m)):
+        final_l.append(m[-1-i])
+    return final_l
+
+#print(l_most_impo_q("bonjour jour doit messieurs abaissement dames le climat change"))
+#print(tri_selec([2,4,4,2,3,6,8,9,10,0,2,1],["II","IV","IV","II","III","VI","VIII","IX","X","zéro","II","I"]))
+
+def respond(text,directory = "./speech/"):
+    #with open(doc_pertinent(TFIDF_Qestion(text)), "r") as f:
+    list_word_impo = l_most_impo_q(text)
+    sentence = ""
+    found = False
+    with open(directory+doc_pertinent(TFIDF_Qestion(text))[8:], "r", encoding="utf-8") as f:
+        file = f.readlines()
+        for word_impo in list_word_impo:
+            for line in file:
+                for word in line.split(" "):
+                    sentence += word + " "
+                    if word_impo in tokenisation(word):
+                        found = True
+                    elif "." in word or "!" in word or "?" in word:
+                        if found:
+                            return sentence
+                        else:
+                            sentence = ""
+            if found:
+                return sentence
+#print(respond("que comptez vous faire pour la crise économique du climat intergalactique"))
+#bonjour jour doit messieurs abaissement dames le climat change
