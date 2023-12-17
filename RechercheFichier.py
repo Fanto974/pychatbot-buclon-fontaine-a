149bb8fc98a4d6e150273a65534462fffb5792e3
JeanKevin56 <148986903+JeanKevin56@users.noreply.github.com>
2023-12-17 21:15:58 +0100

    Adding comments

--- RechercheFichier.py
+++ RechercheFichier.py
@@ -1,7 +1,11 @@
 import os
 import math
 
+#FONCTION FOURNIES DANS L'ENONCE
 def list_of_files(directory, extension):
+    """
+    Renvoie sous forme de liste les fichiers ayant comme extensions la valeur saisies
+    """
     files_names = []
     for filename in os.listdir(directory):
         if filename.endswith(extension):
